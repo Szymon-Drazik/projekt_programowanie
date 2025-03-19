eb785a0fa2d31505923e4b8541d582b68863b5f8
Szymon-Drazik <szymondr033@gmail.com>
2025-03-19 15:31:29 +0100

    baza pierwszy

---
+++ projekt/Nowy folder/Baza.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt.Nowy_folder
+{
+    class Baza
+    {
+        public int Id { get; set; }
+        public string Nazwa_przepisu { get; set; }
+
+        public string Skladniki { get; set; }
+        public string Opis { get; set; }
+        public byte[] Zdjecie { get; set; }
+    }
+}
--- projekt/Window1.xaml.cs
+++ projekt/Window1.xaml.cs
@@ -21,7 +21,7 @@ namespace projekt
     {
         public Window1()
         {
-            InitializeComponent();
+            
         }
     }
 }
